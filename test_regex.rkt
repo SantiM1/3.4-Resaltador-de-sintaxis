eb33168ed10c2d88d4a9a46ff479120377c90808
A00830698 <83973548+SantiM1@users.noreply.github.com>
2022-04-13 23:05:54 -0500

    Primer avance
    Lectura de comentarios de 1 y multilinea. Identificadores var, let y const

--- test_regex.rkt
+++ test_regex.rkt
@@ -4,37 +4,64 @@
 ;Test-Regex
 
 ;JSReader
-(define (JSreader infile outfile)
+(define (JSreader infile ) ;outfile
   (define in(open-input-file infile))
-  (define out(open-output-file outfile))
+  ;(define out(open-output-file outfile))
   (define lst(read_file in))
-  ;(display lst)
-  (html_print lst out)
-  (close-input-port infile)
-  (close-output-port outfile))
+  (display lst)
+  ;(html_print lst out)
+  (close-input-port in))
+  ;(close-output-port out))
 ;read
 (define (read_file file)
   (if (eof-object? (peek-char file))
       (list)
-      (if(equal?(peek-char file) #\space)
-         (read-char file)
-         (if (string-contains? (peek-string 2 0 file) "//")
-             (append(list(append (list(read-line file)) (list" Comentario"))) (read_file file))
-             (append(identify(read-char file)) (read_file file))))))
+      (if(char-whitespace?(peek-char file))
+         (and(read-char file)(read_file file))
+         (if (not(equal?(regexp-match #rx"/\\*" (peek-string 2 0 file)) #f))
+             (append(list(append(regexp-match  #rx"/\\*.*?\\*/" file)(list "Comentario Multilinea")))(read_file file))
+             (if (not(equal?(regexp-match #rx"//" (peek-string 2 0 file)) #f))
+                 (append(list(append(regexp-match #rx"//.*?\n" file)(list "Comentario"))) (read_file file))
+                 (if(not(equal?(regexp-match #rx"var|const|let" (peek-string 5 0 file)) #f))
+                    (append(list(append(regexp-match #rx"var |const |let.*?\n" file) (list "Identificador"))) (read_file file))
+                    (append(list(append(list(read file)) (list "otro"))) (read_file file))))))))
+
 ;html_print
 (define (html_print lst out)
   (list))
+
+
+;ignorar
+; |
+; V
+;mult-line-comment          
 ;identify
-(define (identify ch)
+(define (identify line)
+  (regexp-replace #rx" " line "\n")
+  (define ch(open-input-string line))
+  (identify_helper ch))
+  
+(define (identify_helper ch)
+  (if (eof-object? (peek-char ch))
+      (list)
+      (if(not(equal?(regexp-match #rx"var|const|let" (peek-string 6 0 ch)) #f))
+         (display(append(list(append(regexp-match #rx"var.*\r|const|let" (read-line ch))(list "Identificador")))(identify_helper ch)))
+         (if(not(equal?(regexp-match #rx".=|=|\\+|\\*|/|-|%"  (peek-string 6 0 ch)) #f))
+            (append(list(append(regexp-match #rx".=|=|\\+|\\*|/|-|%" (read-line ch))(list "Operador")))(identify_helper ch))
+            (append(list(read-line ch))(identify_helper ch))))))
+  
+            
+;operation
+(define (operation ch)
   (list))
-#| (if (equal?(regexp-match #rx "=|+|*|..." ch) #f))
-          var
-          (if (operador ch)
-          op
-          (slice ch (regexp-match-positions #rx "=|+|*|..." ch))
+
+;var_list
+(define (var_list ch)
+  (list))
+
+          
+          
           
 
 
 
-
-|#
