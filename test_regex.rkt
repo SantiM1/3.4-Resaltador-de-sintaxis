5d4cffa392a3d298baeff2b77b094b5a90ec32b4
A00830698 <83973548+SantiM1@users.noreply.github.com>
2022-04-17 16:47:40 -0500

    Actualizacion y Caso de prueba 1
    Caso de prueba 1. Actualizacion para strings, numeros 100%, delimitadores y funciones 80%.

--- test_regex.rkt
+++ test_regex.rkt
@@ -8,6 +8,7 @@
   (define in(open-input-file infile))
   ;(define out(open-output-file outfile))
   (define lst(read_file in))
+  (write lst)
   (display lst)
   ;(html_print lst out)
   (close-input-port in))
@@ -18,13 +19,21 @@
       (list)
       (if(char-whitespace?(peek-char file))
          (and(read-char file)(read_file file))
-         (if (not(equal?(regexp-match #rx"/\\*" (peek-string 2 0 file)) #f))
-             (append(list(append(regexp-match  #rx"/\\*.*?\\*/" file)(list "Comentario Multilinea")))(read_file file))
-             (if (not(equal?(regexp-match #rx"//" (peek-string 2 0 file)) #f))
-                 (append(list(append(regexp-match #rx"//.*?\n" file)(list "Comentario"))) (read_file file))
-                 (if(not(equal?(regexp-match #rx"var|const|let" (peek-string 5 0 file)) #f))
-                    (append(list(append(regexp-match #rx"var |const |let.*?\n" file) (list "Identificador"))) (read_file file))
-                    (append(list(append(list(read file)) (list "otro"))) (read_file file))))))))
+         (if (not(equal?(regexp-match #rx"/\\*" (peek-string 2 0 file)) #f)) ;Comentarios Multilinea
+             (append(list(append(regexp-match  #rx"/\\*.*?\\*/" file)(list "Comentario Multilinea-green")))(read_file file))
+             (if (not(equal?(regexp-match #rx"//" (peek-string 2 0 file)) #f)) ;Comentarios 
+                 (append(list(append(regexp-match #rx"//.*?\n" file)(list "Comentario-green"))) (read_file file))
+                 (if(not(equal?(regexp-match #rx"var|const|let|float|int|boolean|return|in" (peek-string 7 0 file)) #f)) ;Palabras reservadas
+                    (append(list(append(regexp-match #rx"var|const|let|float|int|boolean|return|in" file) (list "Identificador-purple"))) (read_file file))
+                    (if(not(equal?(regexp-match #rx"\"" (peek-string 2 0 file)) #f)) ;Strings
+                       (append(list(append(regexp-match #rx"\".*\"" file) (list "String-green"))) (read_file file))
+                       (if(not(equal?(regexp-match #rx"-?[1234567890]" (peek-string 2 0 file)) #f)) ;Numeros
+                          (append(list(append(regexp-match #rx"-?[1234567890]*[\\.e]?-?[1234567890]*" file) (list "Numero -red"))) (read_file file))
+                          (if(not(equal?(regexp-match #rx"if|for" (peek-string 3 0 file)) #f)) ;Delimitadores
+                             (append(list(append(regexp-match #rx"if(.*)\n?{.*}|for(.*)\n?{.*}" file) (list "Delimitadores"))) (read_file file))
+                             (if(not(equal?(regexp-match #rx"function" (peek-string 8 0 file)) #f)) ;Funciones
+                                (append(list(append(regexp-match #rx"function .*(.*?)\n{.*}" file) (list "Funcion"))) (read_file file))
+                                (append(list(append(list(read file)) (list "otro-black"))) (read_file file))))))))))))
 
 ;html_print
 (define (html_print lst out)
@@ -58,6 +67,7 @@
 ;var_list
 (define (var_list ch)
   (list))
+(JSreader "CasoPrueba1.txt")
 
           
           
