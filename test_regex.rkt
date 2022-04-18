6a20ca88391d7d9b771271d6aa22c964ee012fa7
A00830698 <83973548+SantiM1@users.noreply.github.com>
2022-04-17 22:01:35 -0500

    Ordenar lector
    Funciones de verificacion y append regex. Modificacion caso prueba1. Correcciones al codigo test_regex.

--- test_regex.rkt
+++ test_regex.rkt
@@ -18,55 +18,96 @@
   (if (eof-object? (peek-char file))
       (list)
       (if(char-whitespace?(peek-char file))
-         (and(read-char file)(read_file file))
-         (if (not(equal?(regexp-match #rx"/\\*" (peek-string 2 0 file)) #f)) ;Comentarios Multilinea
-             (append(list(append(regexp-match  #rx"/\\*.*?\\*/" file)(list "Comentario Multilinea-green")))(read_file file))
-             (if (not(equal?(regexp-match #rx"//" (peek-string 2 0 file)) #f)) ;Comentarios 
-                 (append(list(append(regexp-match #rx"//.*?\n" file)(list "Comentario-green"))) (read_file file))
-                 (if(not(equal?(regexp-match #rx"var|const|let|float|int|boolean|return|in" (peek-string 7 0 file)) #f)) ;Palabras reservadas
-                    (append(list(append(regexp-match #rx"var|const|let|float|int|boolean|return|in" file) (list "Identificador-purple"))) (read_file file))
-                    (if(not(equal?(regexp-match #rx"\"" (peek-string 2 0 file)) #f)) ;Strings
-                       (append(list(append(regexp-match #rx"\".*\"" file) (list "String-green"))) (read_file file))
-                       (if(not(equal?(regexp-match #rx"-?[1234567890]" (peek-string 2 0 file)) #f)) ;Numeros
-                          (append(list(append(regexp-match #rx"-?[1234567890]*[\\.e]?-?[1234567890]*" file) (list "Numero -red"))) (read_file file))
-                          (if(not(equal?(regexp-match #rx"if|for" (peek-string 3 0 file)) #f)) ;Delimitadores
-                             (append(list(append(regexp-match #rx"if(.*)\n?{.*}|for(.*)\n?{.*}" file) (list "Delimitadores"))) (read_file file))
-                             (if(not(equal?(regexp-match #rx"function" (peek-string 8 0 file)) #f)) ;Funciones
-                                (append(list(append(regexp-match #rx"function .*(.*?)\n{.*}" file) (list "Funcion"))) (read_file file))
-                                (append(list(append(list(read file)) (list "otro-black"))) (read_file file))))))))))))
+         (append(list(read-char file))(read_file file))
+         ;Comentarios Multilinea
+         (if (is_comentario_mult file)             
+             (append(comentario_mult  file)(read_file file))
+              ;Comentarios 
+             (if (is_comentario file)
+                 (append(comentario file)(read_file file))
+                 ;Operadores
+                 (if (is_operador file)
+                    (append(operador file) (read_file file))
+                    ;Identificadores
+                    (if (is_identificador file)                       
+                       (append (identificador file)(read_file file))
+                       ;Palabras Reservadas
+                       (if (is_palabra_reservada file)                         
+                          (append(palabra_reservada file) (read_file file))
+                          ;Strings
+                          (if (is_string file)                             
+                             (append (strings file)(read_file file))
+                             ;Numeros
+                             (if (is_num file)                               
+                                (append (numeros file) (read_file file))
+                                ;Delimitadores
+                                (if (is_del file)
+                                   (append (delimitadores file)(read_file file))
+                                   ;Funciones
+                                   (if (is_func file)
+                                      (append (funciones file) (read_file file))                               
+                                      (append(list(append(regexp-match  #rx".*? " file) (list "Purple"))) (read_file file))))))))))))))
 
 ;html_print
 (define (html_print lst out)
   (list))
 
+;is_comentario_mult
+(define (is_comentario_mult file)
+  (not(equal?(regexp-match #rx"/\\*" (peek-string 2 0 file)) #f)))
+;comentario_mult       
+(define (comentario_mult  file)
+  (list(append(regexp-match  #rx"/\\*.*?\\*/" file)(list "Green"))))
+;is_comentario
+(define (is_comentario file)
+  (not(equal?(regexp-match #rx"//" (peek-string 2 0 file)) #f)))
+;comentario
+(define (comentario file)
+  (list(append(regexp-match #rx"//.*?\n" file)(list "Green"))))
+;is_operator
+(define (is_operador file)
+  (not(equal?(regexp-match #rx"=|!|\\+|\\*|/|-|%|>|<|\\(|\\)|\\[|\\]|{|}|;|,"  (peek-string 1 0 file)) #f)))
+;operator
+(define (operador file)
+  (list(append(regexp-match #rx"===|==|=|!=|!==|\\+=|\\+|\\*=|\\*|\\*{2}=|\\*{2}|/=|/|-=|-|%=|%|>=|>|<=|<|\\(|\\)|\\[|\\]|{|}|;|," file) (list "Black"))))
+;is_identificador
+(define (is_identificador file)
+  (not(equal?(regexp-match #rx"var |const |let " (peek-string 6 0 file)) #f)))
+;identificador
+(define (identificador file)
+  (list(append(regexp-match #rx"var |const |let " file) (list "LightBlue"))))
+;is_palabra_reservada
+(define (is_palabra_reservada file)
+  (not(equal?(regexp-match #rx"float |int |boolean |return |in " (peek-string 8 0 file)) #f)))
+;palabra_reservada
+(define (palabra_reservada file)
+  (list(append(regexp-match #rx"float |int |boolean |return |in " file) (list "Blue"))))
+;is_strings
+(define (is_string file)
+  (not(equal?(regexp-match #rx"\"" (peek-string 2 0 file)) #f)))
+;strings
+(define (strings file)
+  (list(append(regexp-match #rx"\".*\"" file) (list "LightGreen"))))
+;is_num
+(define (is_num file)
+  (not(equal?(regexp-match #rx"-?[1234567890]" (peek-string 2 0 file)) #f)))
+;numeros
+(define (numeros file)
+  (list(append(regexp-match #rx"-?[1234567890]*[\\.e]?-?[1234567890]*" file) (list "Red"))))
+;is_del
+(define (is_del file)
+  (not(equal?(regexp-match #rx"if |for" (peek-string 3 0 file)) #f)))
+;delimitarores
+(define (delimitadores file)
+  (list(append(regexp-match #rx"if |for" file) (list "DarkRed"))))
+;is_func
+(define (is_func file)
+  (not(equal?(regexp-match #rx"function " (peek-string 9 0 file)) #f)))
+;funciones
+(define (funciones file)
+  (list(append(regexp-match #rx"function " file) (list "Orange"))))
 
-;ignorar
-; |
-; V
-;mult-line-comment          
-;identify
-(define (identify line)
-  (regexp-replace #rx" " line "\n")
-  (define ch(open-input-string line))
-  (identify_helper ch))
-  
-(define (identify_helper ch)
-  (if (eof-object? (peek-char ch))
-      (list)
-      (if(not(equal?(regexp-match #rx"var|const|let" (peek-string 6 0 ch)) #f))
-         (display(append(list(append(regexp-match #rx"var.*\r|const|let" (read-line ch))(list "Identificador")))(identify_helper ch)))
-         (if(not(equal?(regexp-match #rx".=|=|\\+|\\*|/|-|%"  (peek-string 6 0 ch)) #f))
-            (append(list(append(regexp-match #rx".=|=|\\+|\\*|/|-|%" (read-line ch))(list "Operador")))(identify_helper ch))
-            (append(list(read-line ch))(identify_helper ch))))))
-  
-            
-;operation
-(define (operation ch)
-  (list))
-
-;var_list
-(define (var_list ch)
-  (list))
+;Execute
 (JSreader "CasoPrueba1.txt")
 
           
